48ea24a9c095cfde6c685cb2bf999f09e8ca7110
Logon Aluno <logonrmlocal@fiap.com.br>
2024-04-19 11:20:39 -0300

    AT-React

--- ATReact/AT-React/src/Componentes/Cards/index.jsx
+++ ATReact/AT-React/src/Componentes/Cards/index.jsx
@@ -1,43 +1,43 @@
-import yt from '/public/Ellipse-youtube.jpg'
-import tk from '/public/Ellipse-tiktok.jpg'
-import fb from '/public/Ellipse-facebook.jpg'
-import ig from '/Ellipse-instagram.jpg'
+import yt from '/public/Ellipse-youtube-removebg-preview.png'
+import tk from '/public/Ellipse-tiktok-removebg-preview.png'
+import fb from '/public/Ellipse-facebook-removebg-preview.png'
+import ig from '/public/Ellipse-instagram-removebg-preview.png'
 
 export default function Cards(){
     return(
         <>
         <div className="All-Cards">
             <div className="card-1">
-                <div>
-                    <h1>01</h1>
-                    <img src={yt} alt="" />
+                    <div className="espaco">
+                        <h1>01</h1>
+                        <img src={yt} alt="" />
+                    </div>
                     <h2>Youtube</h2>
                     <p>Produza conteúdo cativante e conquiste seu público com vídeos de alta qualidade no maior palco digital do mundo.</p>
-                </div>
             </div>
             <div className="card-2">
-                <div>
+                <div className="espaco">
                     <h1>02</h1>
                     <img src={tk} alt="" />
+                </div>
                     <h2>Tiktok</h2>
                     <p>Faça sua criatividade brilhar em vídeos curtos e envolventes que vão cativar a comunidade global do Tiktok.</p>
-                </div>
             </div>
             <div className="card-3">
-                <div>
+                <div className="espaco">
                     <h1>03</h1>
                     <img src={fb} alt="" />
+                </div>
                     <h2>Facebook</h2>
                     <p>Conecte-se com sua audiência de forma autêntica e impactante através de vídeos que se destacam no feed do Facebook.</p>
-                </div>
             </div>
             <div className="card-4">
-                <div>
+                <div className="espaco">
                     <h1>04</h1>
                     <img src={ig} alt="" />
+                </div>
                     <h2>Instagram</h2>
                     <p>Compartilhe suas histórias de maneira única e conquiste milhões de likes no Instagram utilizando Stories e Reels.</p>
-                </div>
             </div>
         </div>
         </>
