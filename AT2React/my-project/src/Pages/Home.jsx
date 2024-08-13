190a20e786e2507c7897182b0a243cb9bb2dcc0b
Laboratório FIAP <labsfiap@fiap.com.br>
2024-08-13 12:54:39 -0300

    AT2React

--- AT2React/my-project/src/Pages/Home.jsx
+++ AT2React/my-project/src/Pages/Home.jsx
@@ -1,22 +1,23 @@
-import { useState, useEffect } from "react"
-import { Link } from "react-router-dom"
-import { data } from "../../artigos.json"
+import data from '../../artigos.json'
 
 export default function Home(){
-
-    console.log(data)
-    const [filmes, setFilmes] = useState([])
-
-    useEffect(()=>{
-        fetch('https://api.themoviedb.org/3/movie/popular?api_key=7c572a9f5b3ba776080330d23bb76e1e')
-        .then(response => response.json())
-        .then(response => setFilmes(response.results))
-        .catch(error => console.log(error))
-    },[])
 
     return(
         <>
         <h1>Home</h1>
+        <div>
+            {
+                (data.map)(
+                    (filme, index)=>(
+                        <>
+                        <div key={index}>
+                            <img src={filme.image}/>
+                        </div>
+                        </>
+                    )
+                )
+            }
+        </div>
         </>
     )
 }
